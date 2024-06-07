c5744060f1fa9269d1bf0d1b1cee105751af36f2
unknown <srinivasmuchu14@gmail.com>
2024-06-07 11:25:02 +0530

    changes

--- src/Components/LoginForms/LoginForm.js
+++ src/Components/LoginForms/LoginForm.js
@@ -1,15 +1,51 @@
 import React from 'react'
-import './LoginForm.css'
+import './LoginForms.css'
+import Topbar from '../TopBar/Topbar'
+import LoginFormLeft from './LoginFormLeft'
+import { useNavigate } from 'react-router-dom'
 
 function LoginForm() {
+  const nav = useNavigate()
   return (
-    <div>
-       <span>Login to your account</span>
-       <div>
-        <input type='text'/>
-       </div>
-       <button>Continue</button>
+    <>
+    <Topbar/>
+     <div className='login-form-page'>
+      <div className='login-form-div'>
+        <LoginFormLeft name='Login'/>
+        <div className='login-form-page-right'>
+          {/* <span className='login-head'>Login to your account</span> */}
+          <div className='login-form-div-cont'>
+            <div className='login-form-input'>
+              <span>Email</span>
+              <input type='text' />
+            </div>
+            <div className='login-form-input'>
+              <span>Password</span>
+              <input type='password' />
+            </div>
+            <div className='login-save-btn'>
+              <button>Login</button>
+            </div>
+
+          </div>
+          {/* <div className='horizontal-line'></div> */}
+          <div className='login-signup-btn'>
+            <div className='signup-btn'>
+              <span>New customer?</span>
+              <button className='signup' onClick={()=>nav('/signup')}>Sign up</button>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+
+
     </div>
+    </>
+   
+
   )
 }
 
---
+++ src/Components/LoginForms/SignUpForm.js
@@ -0,0 +1,89 @@
+import React,{useState} from 'react'
+import './LoginForms.css'
+import Topbar from '../TopBar/Topbar'
+import LoginFormLeft from './LoginFormLeft'
+import { useNavigate } from 'react-router-dom'
+
+function SignUpForm() {
+    const [gender, setGender] = useState('');
+
+    const nav = useNavigate()
+    const handleGenderChange = (event) => {
+        setGender(event.target.value);
+      };
+  return (
+    <>
+    <Topbar/>
+     <div className='login-form-page'>
+      <div className='login-form-div'>
+        <LoginFormLeft name='Signup'/>
+        <div className='login-form-page-right'>
+          {/* <span className='login-head'>Login to your account</span> */}
+          <div className='login-form-div-cont'>
+            <div className='login-form-input'>
+              <span>Email</span>
+              <input type='email' />
+            </div>
+            <div className='login-form-input'>
+              <span>Full name</span>
+              <input type='text' />
+            </div>
+            <div className='login-form-input'>
+              <span>Password</span>
+              <input type='password' />
+            </div>
+            <div className='login-form-input'>
+              <span>Phone number</span>
+              <input type='password' />
+            </div>
+            <div className='login-form-input'>
+            <span>Gender</span>
+            <div style={{display:'flex',alignItems:'center',gap:'10px',width:'80%'}}>
+            <div className='login-form-radio'>
+              <input
+                type='radio'
+                value='male'
+                checked={gender === 'male'}
+                onChange={handleGenderChange}
+               
+              />
+              <span>Male</span>
+            </div>
+            <div className='login-form-radio'>
+              <input
+                type='radio'
+                value='female'
+                checked={gender === 'female'}
+                onChange={handleGenderChange}
+               
+              />
+              <span>Female</span>
+            </div>
+            </div>
+           
+          </div>
+            <div className='login-save-btn'>
+              <button>Sign up</button>
+            </div>
+
+          </div>
+          {/* <div className='horizontal-line'></div> */}
+          <div className='login-signup-btn'>
+            <div className='signup-btn'>
+              <span>Already customer?</span>
+              <button className='signup' onClick={()=>nav('/login')}>Login</button>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+
+
+    </div>
+    </>
+  )
+}
+
+export default SignUpForm
